f8f582ccad8545c0c36e7503d3fbb5547515e389
inspect8543@gmail.com <inspect8543@gmail.com>
2022-11-29 19:05:12 +0900

    cssSettingUpdate

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -9,14 +9,13 @@ export { default as Stacked } from "./Charts/Stacked";
 export { default as GongjoPr } from "./Charts/GongjoPr";
 export { default as DataGrid } from "./Charts/DataGrid";
 export { default as ElectricPr } from "./Charts/ElectricPr";
+// export { default as ChartsHeader } from "./ChartsHeader";
 
 //common
 export { default as Button } from "./common/Button";
 export { default as ThemeSettings } from "./common/ThemeSettings";
 export { default as Sidebar } from "./common/Sidebar";
 export { default as Navbar } from "./common/Navbar";
-export { default as Notification } from "./common/Notification";
-export { default as UserProfile } from "./common/UserProfile";
 export { default as Header } from "./common/Header";
 export { default as DashHeader } from "./common/DashHeader";
 
--- frontend/src/pages/Charts/Area.jsx
+++ frontend/src/pages/Charts/Area.jsx
@@ -34,6 +34,7 @@ const Area = () => {
         chartArea={{ border: { width: 0 } }}
         tooltip={{ enable: true }}
         background={currentMode === "Dark" ? "#33373E" : "#fff"}
+        width="auto"
       >
         <Inject services={[SplineAreaSeries, DateTime, Legend]} />
         <SeriesCollectionDirective>
