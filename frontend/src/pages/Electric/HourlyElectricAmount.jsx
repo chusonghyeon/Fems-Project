fd9b2464a3f0c6b02e784ea47628f5cfb8324336
inspect8543@gmail.com <inspect8543@gmail.com>
2022-12-01 21:12:05 +0900

    maincssChange

--- frontend/src/pages/Electric/HourlyElectricAmount.jsx
+++ frontend/src/pages/Electric/HourlyElectricAmount.jsx
@@ -4,7 +4,7 @@ import Header from "../../components/common/Header";
 import Linearea from "../../components/Charts/Linearea";
 const Electricamount = () => {
   return (
-    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <div className="m-4 md:m-2 mt-24 p-5 bg-white dark:bg-secondary-dark-bg rounded-3xl">
       <Header category="전력량" title="시간별 전력량" />
       <Linearea />
       <DataGrid />
--- frontend/src/pages/Temperature/HourlyTemperature.jsx
+++ frontend/src/pages/Temperature/HourlyTemperature.jsx
@@ -5,11 +5,11 @@ import Header from "../../components/common/Header";
 const HourlyTemperature = () => {
   return (
     <>
-      <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl h-60">
+      <div className="m-4 md:m-2 mt-24 p-5 bg-white dark:bg-secondary-dark-bg rounded-3xl h-60">
         <Header category="온도" title="시간별 온도현황" />
         <Toggleheader />
       </div>
-      <div className="m-4 md:m-10 mt-24 p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg">
+      <div className="m-4 md:m-2 mt-24 p-5 bg-white rounded-3xl dark:bg-secondary-dark-bg">
         <LineChart />
         <TempGrid />
       </div>
