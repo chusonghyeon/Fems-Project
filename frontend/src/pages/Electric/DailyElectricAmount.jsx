7e9a373527696b14fd40ddc6047347fdf84fcaf5
KIM JUN HYEOK <kjh45388117@gmail.com>
2022-12-02 16:26:27 +0900

    API_GRID_CONNET

--- frontend/src/pages/Electric/DailyElectricAmount.jsx
+++ frontend/src/pages/Electric/DailyElectricAmount.jsx
@@ -3,21 +3,41 @@ import { ElectricGrid, Daydate } from "../../components";
 import Header from "../../components/common/Header";
 import Linearea from "../../components/Charts/Linearea";
 const Electricamount = () => {
+  // 클릭시 공조기 ID와 시간정보 출력
+  // const electricHandleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const {
+  //     ahu_id: { value: SelectBox },
+  //     runDate: { value: Hourdate },
+  //   } = e.target;
+
+  //   const ParseHourDate = Hourdate.replaceAll("/", "");
+
+  //   await setStartDate({
+  //     ahu_id: SelectBox,
+  //     runDate: ParseHourDate,
+  //   });
+  //   // 첫번째 {} -> {ahu_id: 'A00', runDate: '20220901'}
+  //   console.log(startDate);
+  // };
+
   return (
     <div className="m-4 md:m-2 mt-24 p-5 bg-white dark:bg-secondary-dark-bg rounded-3xl">
       <Header category="전력량" title="일별 전력량" />
-      <div
-        className="flex mb-10
-      "
-      >
-        <Daydate />
-        <button
-          type="submit"
-          className="pointer w-32 h-auto text-white bg-blue-500 rounded-md"
+      <form>
+        <div
+          className="flex mb-10
+        "
         >
-          검색
-        </button>
-      </div>
+          <Daydate />
+          <button
+            type="submit"
+            className="pointer w-32 h-auto text-white bg-blue-500 rounded-md"
+          >
+            검색
+          </button>
+        </div>
+      </form>
       <Linearea />
       <ElectricGrid />
     </div>
