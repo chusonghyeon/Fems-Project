d715fbf6252b61477a55526b2c8a667d2c3add66
inspect8543@gmail.com <inspect8543@gmail.com>
2022-12-06 14:36:04 +0900

    ahuUpdate

--- frontend/src/pages/Humidity/HourlyHumidity.jsx
+++ frontend/src/pages/Humidity/HourlyHumidity.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { Hourheader, LineChart, TempGrid } from "../../components";
-import Header from "../../components/common/Header";
+import {
+  HumidityGrid,
+  HourHumidityChart,
+  Header,
+  HumidityHourheader,
+} from "../../components";
+
 import { useStateContext } from "../../context/UserContext";
 const HourlyHumidity = () => {
   return (
     <>
       <div className="m-4 md:m-2 mt-24 p-5 w-auto bg-white dark:bg-secondary-dark-bg rounded-3xl h-40">
         <Header category="습도" title="시간별 습도현황" />
-        <Hourheader />
+        <HumidityHourheader />
       </div>
       <div className="m-4 md:m-2 mt-24 p-5 bg-white rounded-3xl dark:bg-secondary-dark-bg">
-        {/* <LineChart /> */}
-        <TempGrid />
+        <HourHumidityChart />
+        <HumidityGrid />
       </div>
     </>
   );
