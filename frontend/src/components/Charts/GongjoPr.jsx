7c6c4f3aab38d75c7212dd71e06f4ab284306f40
inspect8543@gmail.com <inspect8543@gmail.com>
2022-12-08 10:43:40 +0900

    junhuckegaham

--- frontend/src/components/Charts/GongjoPr.jsx
+++ frontend/src/components/Charts/GongjoPr.jsx
@@ -7,65 +7,65 @@ import {
   ColumnSeries,
   Inject,
   Tooltip,
-  DateTime,
+  Category,
 } from "@syncfusion/ej2-react-charts";
 import { Resize } from "@syncfusion/ej2-react-grids";
-import { MultiChartData, MultiChartData2 } from "../../data/dummy";
 import axios from "axios";
+import { GongjoPrimaryXAxis, GongjoPrimaryYAxis } from "../../data/dummy";
 import { GiConsoleController } from "react-icons/gi";
 
 //api 주소
 const SERVER_URL = "/Get_LpData_monthly_Daily_Data";
+
 const GongjoPr = () => {
   const [compareData, setCompareData] = useState([]);
   const CompareChartData = [];
 
   useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(SERVER_URL);
+      setCompareData(res.data);
+    };
+
     fetchData();
-  }, []);
+  }, [setCompareData]);
 
-  const fetchData = async () => {
-    const res = await axios.get(SERVER_URL);
-    setCompareData(res.data);
-    //21
-    let PowerArray = [];
-    const runDate = res.data.slice(0, 11);
-    console.log(runDate);
-    console.log(typeof runDate);
-    runDate.forEach((item) => {
-      let year = item.runDate.slice(0, 4);
-      console.log(year);
-      let month = item.runDate.slice(4, 6);
-      PowerArray.push({
-        x: new Date(year, month),
-        y: item.sumData,
-      });
+  //21
+  let PowerArray = [];
+  const runDate = compareData.slice(0, 11);
+  console.log(runDate);
+  console.log(typeof runDate);
+  runDate.forEach((item) => {
+    let year = item.runDate.slice(0, 4);
+    console.log(year);
+    let month = item.runDate.slice(4, 6);
+    PowerArray.push({
+      x: new Date(year, month),
+      y: item.sumData * 1,
     });
-    CompareChartData.push([...PowerArray]);
+  });
+  CompareChartData.push([...PowerArray]);
 
-    //22
-    PowerArray = [];
-    const runDate2 = res.data.slice(11, 23);
-    console.log(runDate2);
-    console.log(typeof runDate2);
-    runDate2.forEach((item) => {
-      let year = item.runDate.slice(0, 4);
-      let month = item.runDate.slice(4, 6);
-      PowerArray.push({
-        x: new Date(year, month),
-        y: item.sumData,
-      });
+  //22
+  PowerArray = [];
+  const runDate2 = compareData.slice(11, 23);
+  console.log(runDate2);
+  console.log(typeof runDate2);
+  runDate2.forEach((item) => {
+    let year = item.runDate.slice(0, 4);
+    let month = item.runDate.slice(4, 6);
+    PowerArray.push({
+      x: new Date(year, month),
+      y: item.sumData * 1,
     });
-    CompareChartData.push([...PowerArray]);
-    console.log(CompareChartData);
-  };
-
+  });
+  CompareChartData.push([...PowerArray]);
   console.log(CompareChartData);
-
   // 비교차트 2021전력량
   const CompareYearData1 = [
     {
       dataSource: CompareChartData[0],
+      tooltipMappingName: "r",
       type: "Column",
       name: "2021",
       xName: "x",
@@ -76,6 +76,7 @@ const GongjoPr = () => {
   const CompareYearData2 = [
     {
       dataSource: CompareChartData[1],
+      tooltipMappingName: "r",
       type: "Column",
       name: "2022",
       xName: "x",
@@ -91,20 +92,13 @@ const GongjoPr = () => {
         title="월별 전력소모량"
         useGroupingSeparator={true}
         primaryXAxis={{
-          valueType: "DateTime",
-          title: "공조기 데이터",
-          // interval: 1,
-          // labelFormat: "{value}월",
-          labelFormat: "MM월",
-          intervalType: "Months",
-          rangePadding: "None",
-          edgeLabelPlacement: "Shift",
+          GongjoPrimaryXAxis,
         }}
-        primaryYAxis={{ labelFormat: "{value} kWh" }}
+        primaryYAxis={{ GongjoPrimaryYAxis }}
         tooltip={{ enable: true }}
       >
         <Inject
-          services={[ColumnSeries, Legend, Tooltip, Resize, DateTime]}
+          services={[ColumnSeries, Legend, Tooltip, Resize, Category]}
         ></Inject>
         <SeriesCollectionDirective>
           {CompareYearData1.map((item, index) => (
