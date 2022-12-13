7b8454ec54f69b086528bcd33439d6b25ac81532
inspect8543@gmail.com <inspect8543@gmail.com>
2022-12-13 16:18:22 +0900

    basicupdate

--- frontend/src/pages/Mainpage/MainPage.jsx
+++ frontend/src/pages/Mainpage/MainPage.jsx
@@ -9,13 +9,13 @@ const MainPage = () => {
       <div className="w-full mx-4 my-2 md:m-2 px-10 py-5 bg-white dark:bg-secondary-dark-bg rounded-3xl">
         <Header category="MainPage" title="전년도 대비 전력량" />
         <div className="flex justify-center w-full dark:bg-secondary-dark-bg">
-          <GongjoCompare />
+          <ElectricPr />
         </div>
       </div>
       <div className="w-full m-4 md:m-2 px-10 py-5 bg-white dark:bg-secondary-dark-bg rounded-3xl">
         <Header category="" title="예상 전력 소비량" />
         <div className="w-full dark:bg-secondary-dark-bg">
-          <ElectricPr />
+          <GongjoCompare />
         </div>
       </div>
     </div>
